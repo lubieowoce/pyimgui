90a38aa5ff692dabef7c63308b4aa129a79d010e
lubieowoce <lolzatu2@gmail.com>
2018-01-10 21:41:43 +0100

    Let's see if this fixes the combo bug!
    Added missing null at the end of the string, because it's supposed to be
    null terminated

--- imgui/core.pyx
+++ imgui/core.pyx
@@ -2941,6 +2941,7 @@ def combo(str label, int current, list items, int height_in_items=-1):
     cdef int inout_current = current
 
     in_items = "\0".join(items)
+    in_items.append("\0")
 
     return cimgui.Combo(
         _bytes(label), &inout_current, _bytes(in_items), height_in_items
